d1e218520fc0d28f04e158da8e3033d11aafb968
Branislav Trstenský <bt7s7k7@hotmail.sk>
2021-07-09 19:30:47 +0200

    Improved loading indicator

--- src/vue3gui/LoadingIndicator.tsx
+++ src/vue3gui/LoadingIndicator.tsx
@@ -2,12 +2,16 @@ import { defineComponent } from "vue"
 
 export const LoadingIndicator = (defineComponent({
     name: "LoadingIndicator",
+    props: {
+        inline: { type: Boolean },
+        prewarm: { type: Boolean },
+    },
     setup(props, ctx) {
         return () => (
-            <div class="as-loading-indicator">
-                <div><div>•</div></div>
-                <div><div>•</div></div>
-                <div><div>•</div></div>
+            <div class={["as-loading-indicator", props.inline && "inline", props.prewarm && "prewarm"]}>
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
         )
     }
