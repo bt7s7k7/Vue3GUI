f763639a45724d4f7177edab38612de0ca48a264
Branislav Trstenský <bt7s7k7@hotmail.sk>
2021-07-12 19:28:54 +0200

    Textfiled autoresize

--- src/vue3gui/TextField.tsx
+++ src/vue3gui/TextField.tsx
@@ -18,7 +18,8 @@ export const TextField = eventDecorator(defineComponent({
             type: String as PropType<Variant>,
             default: () => Theme.selected.highlight
         },
-        focus: { type: Boolean }
+        focus: { type: Boolean },
+        autoResize: { type: Boolean }
     },
     emits: {
         "update:modelValue": (value: string) => true,
@@ -32,26 +33,48 @@ export const TextField = eventDecorator(defineComponent({
 
         onMounted(() => {
             if (props.focus) input.value?.focus()
+            setTimeout(() => {
+                if (props.autoResize && input.value) autoResize()
+            }, 10)
         })
 
         watch(() => props.modelValue, (newValue) => {
             value.value = newValue
         })
 
+        let measureElement: HTMLSpanElement | null = null
+        const autoResize = () => {
+            const inputElement = input.value!
+            const container = inputElement.parentElement!
+
+            if (!measureElement) measureElement = document.createElement("span")
+            measureElement.innerText = value.value || "0"
+            container.appendChild(measureElement)
+            const width = measureElement.getBoundingClientRect().width
+            container.removeChild(measureElement)
+
+            container.style.width = `${width + 5}px`
+        }
+
         watch(value, (value) => {
             ctx.emit("update:modelValue", value)
             ctx.emit("input", value)
+
+            if (props.autoResize) {
+                autoResize()
+            }
         })
 
         return () => (
             <div class="flex row as-text-field">
                 <input
                     type={props.type}
-                    onKeydown={e => e.code == "Enter" && (ctx.emit("confirm", value.value), ctx.emit("change", value.value))}
+                    onKeydown={e => (e.code == "Enter" || e.code == "NumpadEnter") && (ctx.emit("confirm", value.value), ctx.emit("change", value.value))}
                     onBlur={() => ctx.emit("change", value.value)}
                     v-model={value.value}
                     class="flex-fill"
                     ref={input}
+                    size={1}
                 />
                 <div class={["focus-indicator", `bg-${props.variant}`]}></div>
             </div>
