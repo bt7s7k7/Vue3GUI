1e7efe3c67e6f1a0489a1ab3775e237ab6d182fb
Branislav Trstenský <bt7s7k7@hotmail.sk>
2021-08-17 11:56:29 +0200

    Added useAbsoluteResize hook

---
+++ src/vue3gui/useAbsoluteResize.ts
@@ -0,0 +1,45 @@
+import { css } from "@emotion/css"
+import { computed, onMounted, reactive, ref } from "vue"
+
+export function useAbsoluteResize(strategy: "max" | "last" | "first" = "max") {
+    const transitionClass = css({
+        transition: "height 0.1s ease-in-out"
+    })
+
+    const height = ref(0)
+    const target = ref<HTMLElement>()
+    function recalculate() {
+        const container = target.value!
+
+        if (strategy == "max") {
+            let max = 0
+            for (const child of Array.from(container.children)) {
+                max = Math.max(max, child.getBoundingClientRect().height)
+            }
+
+            height.value = max
+        } else if (strategy == "first") {
+            height.value = container.children[0]?.getBoundingClientRect().height ?? 0
+        } else if (strategy == "last") {
+            height.value = container.children[container.children.length - 1]?.getBoundingClientRect().height ?? 0
+        }
+    }
+
+    onMounted(() => {
+        recalculate()
+        setTimeout(() => {
+            recalculate()
+        }, 110)
+    })
+
+    const ret = {
+        targetProps: {
+            ref: target,
+            class: transitionClass,
+            style: reactive({ height: computed(() => height.value + "px") })
+        },
+        recalculate
+    }
+
+    return ret
+}
