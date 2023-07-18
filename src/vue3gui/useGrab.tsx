0727881de85a80c8bca39b2398d76053279a7834
Branislav Trstenský <bt7s7k7@hotmail.sk>
2023-07-18 14:53:16 +0200

    Touch support for useGrab

--- src/vue3gui/useGrab.tsx
+++ src/vue3gui/useGrab.tsx
@@ -1,10 +1,15 @@
-interface GrabMoveEvent {
+export interface GrabMoveEvent {
     startX: number
     startY: number
     currentX: number
     currentY: number
     deltaX: number
     deltaY: number
+}
+
+interface HandlerInput {
+    pageX: number
+    pageY: number
 }
 
 export function useGrab<T = null>({
@@ -18,16 +23,25 @@ export function useGrab<T = null>({
     let startX = 0
     let startY = 0
 
-    return (event: MouseEvent) => {
+    return (event: MouseEvent | TouchEvent) => {
         if (dragging) return
-        if (event.button != button) return
+        if (event instanceof MouseEvent) if (event.button != button) return
 
         event.preventDefault()
         event.stopPropagation()
 
-        startX = event.pageX
-        startY = event.pageY
+        let touch: Touch = null!
+        if (event instanceof MouseEvent) {
+            startX = event.pageX
+            startY = event.pageY
+        } else {
+            touch = event.changedTouches.item(0)!
+            startX = touch.pageX
+            startY = touch.pageY
+        }
+
         dragging = true
+
         const state = onMoveStart({ startX, startY, currentX: startX, currentY: startY, deltaX: 0, deltaY: 0 })
 
         const element = document.createElement("div")
@@ -35,26 +49,60 @@ export function useGrab<T = null>({
         element.classList.add("as-fullscreen-overlay")
         element.style.cursor = cursor
 
-        const moveListener = (event: MouseEvent) => {
+        const moveHandler = (event: HandlerInput) => {
             const currentX = event.pageX
             const currentY = event.pageY
             onMove({ startX, startY, currentX, currentY, deltaX: currentX - startX, deltaY: currentY - startY }, state)
         }
-        const upListener = (event: MouseEvent) => {
-            if (event.button != button)
-                return
+
+        const upHandler = (event: HandlerInput) => {
             element.remove()
             dragging = false
 
             const currentX = event.pageX
             const currentY = event.pageY
             onMoveEnd({ startX, startY, currentX, currentY, deltaX: currentX - startX, deltaY: currentY - startY }, state)
-
-            window.removeEventListener("mousemove", moveListener)
-            window.removeEventListener("mouseup", upListener)
         }
 
-        window.addEventListener("mousemove", moveListener)
-        window.addEventListener("mouseup", upListener)
+        if (event instanceof MouseEvent) {
+            const moveListener = (event: MouseEvent) => {
+                moveHandler(event)
+            }
+
+            const upListener = (event: MouseEvent) => {
+                if (event.button != button)
+                    return
+
+                upHandler(event)
+
+                window.removeEventListener("mousemove", moveListener)
+                window.removeEventListener("mouseup", upListener)
+            }
+
+            window.addEventListener("mousemove", moveListener)
+            window.addEventListener("mouseup", upListener)
+        } else if (event instanceof TouchEvent) {
+            const moveListener = (event: TouchEvent) => {
+                const newTouch = event.changedTouches.item(0)!
+                if (newTouch.identifier != touch.identifier) return
+                touch = newTouch
+
+                moveHandler(touch)
+            }
+
+            const upListener = (event: TouchEvent) => {
+                const newTouch = event.changedTouches.item(0)!
+                if (newTouch.identifier != touch.identifier) return
+                touch = newTouch
+
+                upHandler(touch)
+
+                window.removeEventListener("touchmove", moveListener)
+                window.removeEventListener("touchend", upListener)
+            }
+
+            window.addEventListener("touchmove", moveListener)
+            window.addEventListener("touchend", upListener)
+        }
     }
 }
