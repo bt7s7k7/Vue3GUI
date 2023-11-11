6d3bb46000218edc28b519e143f54494b33ff165
Branislav Trstenský <bt7s7k7@hotmail.sk>
2023-11-11 19:02:06 +0100

    MountNode

---
+++ src/vue3gui/MountNode.tsx
@@ -0,0 +1,49 @@
+import { ComponentInternalInstance, PropType, createCommentVNode, defineComponent, getCurrentInstance, onMounted, onUpdated, watch } from "vue"
+
+export const MountNode = (defineComponent({
+    name: "MountElement",
+    props: {
+        node: { type: Node as PropType<Node | null> }
+    },
+    setup(props, ctx) {
+        let instance: ComponentInternalInstance | null = null
+        let lastNode: Node | null = null
+
+        function update() {
+            if (instance == null) return null
+
+            const node = props.node
+            const element = instance.vnode.el
+
+            if (element == null) {
+                lastNode?.parentNode?.removeChild(lastNode)
+                lastNode = null
+                return
+            }
+
+            if (lastNode != null && lastNode != node) {
+                lastNode.parentNode?.removeChild(lastNode)
+            }
+
+            if (!(element instanceof Comment)) throw new Error("MountNode root is not a comment")
+            if (!element.parentElement) return
+
+            if (node != null) {
+                element.parentElement.insertBefore(node, element.nextSibling)
+            }
+            lastNode = node ?? null
+        }
+
+        onMounted(() => {
+            instance = getCurrentInstance()
+            update()
+        })
+
+        watch(() => props.node, update)
+        onUpdated(update)
+
+        return () => (
+            createCommentVNode("MountNode")
+        )
+    }
+}))
