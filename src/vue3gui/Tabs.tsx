9d14c31443003588b78456a8ca39a45a646de306
Branislav Trstenský <bt7s7k7@hotmail.sk>
2024-01-29 15:45:35 +0100

    TabbedContainer

--- src/vue3gui/Tabs.tsx
+++ src/vue3gui/Tabs.tsx
@@ -1,5 +1,6 @@
-import { defineComponent, h, onMounted, PropType, reactive, watch } from "vue"
+import { defineComponent, ExtractPropTypes, h, nextTick, onMounted, PropType, reactive, watch } from "vue"
 import { Button } from "./Button"
+import { NATIVE_EVENTS, normalizeVNodeChildren } from "./util"
 import { Variant } from "./variants"
 import { useTheme } from "./vue3gui"
 
@@ -14,7 +15,9 @@ interface TabsStateBase {
     list: [string, any][]
 }
 
-export function useTabs<T extends Record<string, any>>(tabs: T, defaultValue: keyof T | null = null) {
+export function useTabs<T extends Record<string, any>>(tabs: T, defaultValue?: keyof T | null): TypedTabsState<T>
+export function useTabs(): TabsStateBase
+export function useTabs<T extends Record<string, any>>(tabs: T = {} as T, defaultValue: keyof T | null = null) {
     const state = reactive<TypedTabsState<T>>({
         selected: defaultValue ?? Object.keys(tabs)[0],
         list: Object.entries(tabs),
@@ -24,35 +27,43 @@ export function useTabs<T extends Record<string, any>>(tabs: T, defaultValue: ke
         }
     })
 
-    return state
+    return state as any
 }
+
+const _TABS_PROPS = {
+    variant: {
+        type: String as PropType<Variant>,
+    },
+    tabs: {
+        type: Object as PropType<TabsStateBase>,
+        required: true as const
+    },
+    compact: { type: Boolean },
+    buttonClass: { type: String }
+}
+type _TabsProps = ExtractPropTypes<typeof _TABS_PROPS>
 
 export const Tabs = (defineComponent({
     name: "Tabs",
-    props: {
-        variant: {
-            type: String as PropType<Variant>,
-        },
-        tabs: {
-            type: Object as PropType<TabsStateBase>,
-            required: true
-        },
-        compact: { type: Boolean },
-        buttonClass: { type: String }
-    },
+    props: _TABS_PROPS,
     setup(props, ctx) {
         const { theme } = useTheme()
 
         const indicators: Record<string, HTMLDivElement> = {}
 
-        watch(() => props.tabs.selected, (selected, oldSelected) => {
+        function doIndicatorAnimation(selected: string | null, oldSelected: string | null, deferred: boolean) {
             if (!selected) throw new Error("Selected state changed to null")
 
             if (!oldSelected) {
+                if (!(selected in indicators)) {
+                    if (deferred) throw new Error("Selected tab does not exist")
+                    nextTick(() => doIndicatorAnimation(selected, oldSelected, true))
+                    return
+                }
+
                 indicators[selected].classList.add("active")
                 return
             }
-
 
             const oldIndicator = indicators[oldSelected]
             const targetIndicator = indicators[selected]
@@ -75,6 +86,12 @@ export const Tabs = (defineComponent({
                     targetIndicator.classList.remove("moving")
                 }, 100)
             }, 10)
+        }
+
+        watch(() => props.tabs.selected, (selected, oldSelected) => {
+            if (props.tabs.list.length == 0) return
+
+            doIndicatorAnimation(selected, oldSelected, false)
         })
 
         onMounted(() => {
@@ -96,6 +113,93 @@ export const Tabs = (defineComponent({
                     </Button>
                 ))}
             </div>
+        )
+    }
+}))
+
+interface _ChildTab {
+    name: string
+    label: string
+    content: any
+}
+
+export const TabbedContainer = (defineComponent({
+    name: "TabbedContainer",
+    props: {
+        defaultValue: { type: String },
+        tabsProps: { type: Object as PropType<Omit<_TabsProps, "tabs"> & { class: any, style: any }> },
+        tabsWrapper: { type: Function as PropType<(vnode: any) => any> },
+        externalTabs: { type: Object as PropType<TabsStateBase> },
+        ...NATIVE_EVENTS
+    },
+    setup(props, ctx) {
+        function getChildren() {
+            const children = normalizeVNodeChildren(ctx.slots.default?.())
+            const tabs: _ChildTab[] = []
+
+            for (const child of children) {
+                if (typeof child == "object" && child != null && child.type == Tab) {
+                    if (child.props == null || child.props.name == null) continue
+
+                    const name = child.props.name
+                    const label = child.props.label ?? name
+                    tabs.push({ name, label, content: child })
+                }
+            }
+
+            return tabs
+        }
+
+        function updateTabList(children: _ChildTab[]) {
+            const oldTabs = tabs.list
+            const newTabs: typeof oldTabs = children.map(v => [v.name, v.label])
+            if (oldTabs.map(v => v[0]).join(",") != newTabs.map(v => v[0]).join(",")) {
+                nextTick(() => {
+                    if (!newTabs.some(v => v[0] == tabs.selected)) {
+                        tabs.selected = newTabs[0]?.[0] ?? ""
+                    }
+
+                    tabs.list = newTabs
+                })
+            }
+
+        }
+
+        const tabs = props.externalTabs ?? useTabs({}) as TabsStateBase
+
+        return () => {
+            const children = getChildren()
+            updateTabList(children)
+            const selected = children.find(v => v.name == tabs.selected)
+            const content = props.tabsWrapper ? props.tabsWrapper(selected?.content) : selected?.content
+
+            if (props.externalTabs) {
+                return (
+                    <div>
+                        {content}
+                    </div>
+                )
+            } else {
+                return (
+                    <div>
+                        <Tabs {...props.tabsProps} tabs={tabs} />
+                        {content}
+                    </div>
+                )
+            }
+        }
+    }
+}))
+
+export const Tab = (defineComponent({
+    name: "TabbedContainer",
+    props: {
+        name: { type: String, required: true },
+        label: { type: String },
+    },
+    setup(props, ctx) {
+        return () => (
+            ctx.slots.default?.()
         )
     }
 }))
