7900d5501939655e862b3252bfbef215863b3004
Branislav Trstenský <bt7s7k7@hotmail.sk>
2024-10-25 17:44:55 +0200

    More menu methods

--- src/vue3gui/Menu.tsx
+++ src/vue3gui/Menu.tsx
@@ -3,7 +3,7 @@ import { ButtonProps } from "./Button"
 import { MenuContent, MenuItem, MenuItemProps } from "./MenuItem"
 
 export abstract class Menu<T> {
-    public abstract getItems(): T[]
+    public abstract getItems(): readonly T[]
     public abstract getKey(item: T): string
 
     public getLabel(item: T): any {
@@ -18,22 +18,31 @@ export abstract class Menu<T> {
         return null
     }
 
+    public isSelected(item: T): boolean {
+        return false
+    }
+
+    public onClick(item: T) {
+
+    }
+
     public renderItem(item: T) {
         const content = this.getContent(item)
         const key = this.getKey(item)
         const label = this.getLabel(item)
+        const selected = this.isSelected(item)
 
-        return <MenuItem key={key}>
+        return <MenuItem key={key} selected={selected} onClick={() => this.onClick(item)} {...this.getProps(item)}>
             {label}
             {content && <MenuContent>{content}</MenuContent>}
         </MenuItem>
     }
 
-    public getProps(item: T): MenuItemProps.Function & ButtonProps.Function {
-        return {}
+    public getProps(item: T): (MenuItemProps.Function & ButtonProps.Function) | undefined {
+        return undefined
     }
 
-    public renderItems(items: T[]) {
+    public renderItems(items: readonly T[]) {
         return <>
             {items.map(item => {
                 const children = this.getNestedItems(item)
@@ -55,7 +64,7 @@ export abstract class Menu<T> {
         return this.renderItems(this.getItems())
     }
 
-    public getNestedItems(item: T): T[] | null {
+    public getNestedItems(item: T): readonly T[] | null {
         return []
     }
 
