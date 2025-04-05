748e429a66f4ee95c8cd6427b01a549273ba0b54
Branislav Trstenský <bt7s7k7@hotmail.sk>
2025-04-05 19:20:36 +0200

    v-label directive

--- src/vue3gui/vue3gui.ts
+++ src/vue3gui/vue3gui.ts
@@ -1,4 +1,4 @@
-import { stringifyStyle } from "@vue/shared"
+import { escapeHtml, stringifyStyle } from "@vue/shared"
 import { App, DirectiveBinding, normalizeClass, normalizeStyle } from "vue"
 
 function* _applyChildDirective(el: unknown, binding: DirectiveBinding) {
@@ -49,12 +49,24 @@ function* _applyChildDirective(el: unknown, binding: DirectiveBinding) {
     }
 }
 
+function _addClass(element: Element, classToAdd: string) {
+    const classname = normalizeClass([element.getAttribute("class"), classToAdd])
+    element.setAttribute("class", [...new Set(classname.split(" "))].join(" "))
+}
+
+const _LABEL_HOST = Symbol.for("vue3gui.labelHost")
+const _LABEL_ALIGNMENTS: Record<string, [string, string]> = {
+    "bottom": ["top-100 left-0 w-fill h-0 flex center", "top-100 mt-1"],
+    "top": ["bottom-100 left-0 w-fill h-0 flex center", "bottom-100 mb-1"],
+    "right": ["left-100 top-0 h-fill w-0 flex center", "left-100 ml-1"],
+    "left": ["right-100 top-0 h-fill w-0 flex center", "right-100 mr-1"],
+}
+
 export const vue3gui = {
     install(app: App, options: {}) {
         app.directive("child-class", (el, binding, vnode, prevVNode) => {
             for (const child of _applyChildDirective(el, binding)) {
-                const classname = normalizeClass([child.getAttribute("class"), binding.value])
-                child.setAttribute("class", [...new Set(classname.split(" "))].join(" "))
+                _addClass(child, binding.value)
             }
         })
 
@@ -63,6 +75,39 @@ export const vue3gui = {
                 const style = normalizeStyle([child.getAttribute("style"), binding.value])
                 child.setAttribute("style", stringifyStyle(style))
             }
+        })
+
+        app.directive("label", (el, binding, vnode, prevVNode) => {
+            if (!(el instanceof HTMLElement)) {
+                throw new TypeError("Can only use v-affect on HTMLElement")
+            }
+
+            _addClass(el, "hover-check")
+
+            const prevLabel = el.ariaLabel
+            const label = binding.value as string
+            if (prevLabel == label) return
+
+            el.ariaLabel = label
+
+            let labelHost: [HTMLElement, HTMLElement] | null = (el as any)[_LABEL_HOST]
+            if (labelHost == null) {
+                labelHost = [document.createElement("div"), document.createElement("div")];
+                (el as any)[_LABEL_HOST] = labelHost
+
+                el.appendChild(labelHost[0])
+                labelHost[0].appendChild(labelHost[1])
+
+                labelHost[0].style.zIndex = "1000"
+            }
+
+            const alignment = _LABEL_ALIGNMENTS[binding.arg ?? "bottom"]
+            if (alignment == null) throw new TypeError(`Invalid label alignment "${binding.arg}"`)
+
+            labelHost[0].className = "if-hover-fade fade-delay absolute " + alignment[0]
+            labelHost[1].className = "small rounded shadow bg-default absolute p-1 px-2 nowrap ignored " + alignment[1]
+
+            labelHost[1].innerHTML = escapeHtml(label)
         })
     },
 }
